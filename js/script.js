9b1b0ff709f948bd0f805719c763bc8af02f0c14
syahrul <syahrulrangga10@gmail.com>
2024-08-07 17:10:22 +0700

    WEB JHS 4x4

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active //
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di click //
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// click di luar sidebar untuk menghilangkan nav //
+const menu = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
